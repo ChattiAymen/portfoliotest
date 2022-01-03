142708bc1a4d7e329f15bc4c8607d656fcd8821a
Chatti Aymen <chatti.aymen.pro@gmail.com>
2022-01-03 18:15:21 +0100

    .

---
+++ src/PorfolioContainer/Home/Profile.js
@@ -0,0 +1,76 @@
+import React from "react";
+import Typical from "react-typical";
+import "./Profile.css";
+
+export default function Profile() {
+  return (
+    <div className="profile-container">
+      <div className="profile-parent">
+        <div className="profile-details">
+          <div className="colz">
+            <div className="colz-icon">
+              <a href="https://www.facebook.com/aymen.chatti/">
+                <i className="fa fa-facebook-square"></i>
+              </a>
+              <a href="#">
+                <i className="fa fa-google-plus-square"></i>
+              </a>
+              <a href="https://www.instagram.com/chatti_ayymen/">
+                <i className="fa fa-instagram"></i>
+              </a>
+              <a href="https://www.youtube.com/channel/UCAfsriS3_aOLqp2tgo1LgJw">
+                <i className="fa fa-youtube-square"></i>
+              </a>
+              <a href="https://twitter.com/ChattiAymen3">
+                <i className="fa fa-twitter"></i>
+              </a>
+            </div>
+          </div>
+
+          <div className="profile-details-name">
+            <span className="primary-text">
+              {" "}
+              Hello, I'M <span className="highlighted-text">CHATTI AYMEN</span>
+            </span>
+          </div>
+          <div className="profile-details-role">
+            <span className="primary-text">
+              {" "}
+              <h1>
+                {" "}
+                <Typical
+                  loop={Infinity}
+                  steps={[
+                    "Ethusiastic Dev 🔴",
+                    1000,
+                    "FullStack Developer 💻",
+                    1000,
+                    "MERN Stack Dev 😎",
+                    1000,
+                    "Cross Platform v 🌍",
+                    1000,
+                    "React/React Native Dev 📱",
+                    1000,
+                  ]}
+                />
+              </h1>
+              <span className="profile-role-tagline">
+                Knack of building applications with front and back end
+                operations.
+              </span>
+            </span>
+          </div>
+          <div className="profile-options">
+            <button className="btn primary-btn">{""} Hire Me </button>
+            <a href="Chatti.pdf" download="Chatti.pdf">
+              <button className="btn highlighted-btn"> Get Resume</button>
+            </a>
+          </div>
+        </div>
+        <div className="profile-picture">
+          <div className="profile-picture-background"></div>
+        </div>
+      </div>
+    </div>
+  );
+}
